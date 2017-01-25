0db442d523eb98398d93f7b495231a0b60b0945c
SamuelOng <SamuelOng@DESKTOP-K99BH7H>
2017-01-25 22:37:05 +0800

    Updated all the classes.

---
+++ Movie_Ticketing_System/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movie_Ticketing_System
+{
+    class Movie
+    {
+        public string Title { get; set; }
+        public int Duration { get; set; }
+        public string Classification { get; set; }
+        public DateTime OpeningDate { get; set; }
+        public List<string> GenreList { get; set; } = new List<string>();
+
+        public Movie() { }
+        public Movie(string title, int duration, string classification, DateTime openingdate, List<string> genrelist)
+        {
+            Title = title;
+            Duration = duration;
+            Classification = classification;
+            OpeningDate = openingdate;
+            GenreList = genrelist;
+        }
+        public override string ToString()
+        {
+            return String.Format("{0,-35}{1,-9}{2,-20}{3,-14}{4}", Title, Duration, Classification, OpeningDate);
+        }
+    }
+}
---
+++ Movie_Ticketing_System/Screening.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movie_Ticketing_System
+{
+    class Screening
+    {
+        private static int screeningCount { get; set; } = 1001;
+        public string ScreeningNo { get; set; }
+        public DateTime ScreeningDateTime { get; set; }
+        private string screeningType;
+        public string ScreeningType
+        { 
+            get { return ScreeningType; }
+            set { if (value == "2D" || value == "3D") screeningType = value; }
+        }
+
+
+        public int SeatsRemaining { get; set; }
+        public CinemaHall CinemaHall { get; set; }
+        public Movie Movie { get; set; }
+
+        public Screening() { }
+        public Screening(DateTime screeningdatetime, string screeningtype, CinemaHall cinemahall, Movie movie)
+        {
+            ScreeningNo = Convert.ToString(screeningCount);
+            screeningCount++;
+            ScreeningDateTime = screeningdatetime;
+            ScreeningType = screeningType;
+            CinemaHall = cinemahall;
+            Movie = movie;
+        }
+        public override string ToString()
+        {
+            return "ScreeningDateTime: " + ScreeningDateTime + " Type: " + ScreeningType + " Cinema Hall: " + CinemaHall + " Movie " + Movie;
+        }
+    }
+}
